0b7de192a1fd92ed2c03d2dcf20654b85635bc6f
bennybop <bennybop74@yahoo.com>
2018-01-12 07:53:14 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,4 @@
 def unsafe?(speed)
-
 	if speed > 60
 		 true
 	elsif speed < 40
@@ -8,15 +7,14 @@ def unsafe?(speed)
 		 false
 
 	end
-
 end
 
 
 
+
+
+
+
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : false
-
-
-
-
+speed > 60 ? true || speed < 40 ? true : false 
 end
